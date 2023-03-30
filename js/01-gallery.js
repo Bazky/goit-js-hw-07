30d8cdaa6bd8ca587f4ef0ac66d1dfb80be0c5be
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-03-30 14:43:56 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,74 +1,43 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryItems = [
-  {
-    preview: "img/preview-1.jpg",
-    original: "img/original-1.jpg",
-    description: "Description 1",
-  },
-  {
-    preview: "img/preview-2.jpg",
-    original: "img/original-2.jpg",
-    description: "Description 2",
-  },
-  {
-    preview: "img/preview-3.jpg",
-    original: "img/original-3.jpg",
-    description: "Description 3",
-  },
-  {
-    preview: "img/preview-4.jpg",
-    original: "img/original-4.jpg",
-    description: "Description 4",
-  },
-  {
-    preview: "img/preview-5.jpg",
-    original: "img/original-5.jpg",
-    description: "Description 5",
-  },
-  {
-    preview: "img/preview-6.jpg",
-    original: "img/original-6.jpg",
-    description: "Description 6",
-  },
-];
 
-const gallery = document.querySelector(".gallery");
-
-const galleryItemTemplate = ({ preview, original, description }) => `
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
-      </a>
-    </div>
-  `;
-
-const galleryMarkup = galleryItems.map(galleryItemTemplate).join("");
-
-gallery.insertAdjacentHTML("beforeend", galleryMarkup);
-
-gallery.addEventListener("click", onGalleryClick);
-
-function onGalleryClick(event) {
-  event.preventDefault();
-
-  const { target } = event;
-
-  if (target.nodeName !== "IMG") {
-    return;
-  }
-
-  const basicLightboxInstance = basicLightbox.create(`
-      <img src="${target.dataset.source}" alt="${target.alt}" />
-    `);
-
-  basicLightboxInstance.show();
-}
 import * as basicLightbox from "basiclightbox";
+import "basiclightbox/dist/basicLightbox.min.css";
 
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`);
+import { galleryItems } from "./gallery-items";
 
-instance.show();
+const galleryList = document.querySelector(".gallery__list");
+
+galleryItems.forEach((item) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.setAttribute("href", item.original);
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.setAttribute("src", item.preview);
+  galleryImage.setAttribute("data-source", item.original);
+  galleryImage.setAttribute("alt", item.description);
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+  galleryList.appendChild(galleryItem);
+});
+
+galleryList.addEventListener("click", (e) => {
+  e.preventDefault();
+  const target = e.target;
+
+  if (target.classList.contains("gallery__image")) {
+    const imageSrc = target.dataset.source;
+    const instance = basicLightbox.create(`
+      <img src="${imageSrc}" alt="" width="800" height="600">
+    `);
+    instance.show();
+  }
+});
+
 console.log(galleryItems);
