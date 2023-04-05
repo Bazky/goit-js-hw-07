b9c55c76945b2a2461003ca02ac2eacbf64c3e0b
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-04-05 19:02:22 +0200

    3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const li = document.createElement("li");
+  li.classList.add("gallery__item");
+
+  const a = document.createElement("a");
+  a.classList.add("gallery__link");
+  a.href = item.fullview;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = item.preview;
+  img.alt = item.alt;
+
+  const caption = document.createElement("div");
+  caption.classList.add("gallery__caption");
+  caption.textContent = item.alt;
+}
 console.log(galleryItems);
