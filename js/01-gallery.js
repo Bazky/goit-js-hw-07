dcad95655be65b5d9d321c09273a2fd3f3f1e163
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-04-06 16:00:45 +0200

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 
-const gallery = document.querySelector(".gallery");
-
 function renderGalleryItems() {
   const galleryItemsElements = galleryItems.map(createGalleryItem);
 }
