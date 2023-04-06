9203a47804f1bee12cdf4afe8a2d5a687be12f8f
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-04-06 15:44:11 +0200

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,22 @@
 import { galleryItems } from "./gallery-items.js";
 
-import * as basicLightbox from "basiclightbox";
 const gallery = document.querySelector(".gallery");
 
 function renderGalleryItems() {
   const galleryItemsElements = galleryItems.map(createGalleryItem);
-  function createGalleryItem(item) {
-    const galleryItem = document.querySelector(".gallery__item");
-    const image = galleryItem.querySelector(".gallery__image");
-    const link = galleryItem.querySelector(".gallery__link");
+}
 
-    image.src = item.preview;
-    image.dataset.source = item.original;
-    image.alt = item.description;
-    link.href = item.original;
+function createGalleryItem(item) {
+  const galleryItem = document.querySelector(".gallery__item");
+  const image = galleryItem.querySelector(".gallery__image");
+  const link = galleryItem.querySelector(".gallery__link");
 
-    return galleryItem;
-  }
+  image.src = item.preview;
+  image.dataset.source = item.original;
+  image.alt = item.description;
+  link.href = item.original;
+
+  return galleryItem;
 }
 
 function onGalleryItemClick(event) {
