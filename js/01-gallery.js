c074396de8bad565bf3f793b1b635c2359687507
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-04-06 15:16:29 +0200

    5

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,48 +1,3 @@
-// import { galleryItems } from "./gallery-items.js";
-
-// import * as basicLightbox from "basiclightbox";
-// const gallery = document.querySelector(".gallery");
-
-// function renderGalleryItems(item) {
-//   const galleryItemsElements = galleryItems.map(createGalleryItem);
-//   function createGalleryItem() {
-//     const galleryItem = document.querySelector(".gallery__item");
-//     const image = galleryItem.querySelector(".gallery__image");
-//     const link = galleryItem.querySelector(".gallery__link");
-
-//     image.src = item.preview;
-//     image.dataset.source = item.original;
-//     image.alt = item.description;
-//     link.href = item.original;
-
-//     return galleryItem;
-//   }
-// }
-
-// function onGalleryItemClick(event) {
-//   event.preventDefault();
-//   const imageSrc = event.target.dataset.source;
-//   const instance = basicLightbox.create(`
-//     <img src="${imageSrc}" width="800" height="600">
-//   `);
-//   instance.show();
-// }
-
-// function addGalleryItemsListener() {
-//   gallery.addEventListener("click", (event) => {
-//     if (event.target.classList.contains("gallery__image")) {
-//       onGalleryItemClick(event);
-//     }
-//   });
-// }
-
-// renderGalleryItems();
-// createGalleryItem();
-// onGalleryItemClick();
-// addGalleryItemsListener();
-
-// console.log(galleryItems);
-
 import { galleryItems } from "./gallery-items.js";
 
 import * as basicLightbox from "basiclightbox";
@@ -59,10 +14,6 @@ function renderGalleryItems() {
     image.dataset.source = item.original;
     image.alt = item.description;
     link.href = item.original;
-
-    // Set the maximum width and height of the image
-    image.style.maxWidth = "800px";
-    image.style.maxHeight = "600px";
 
     return galleryItem;
   }
@@ -86,5 +37,8 @@ function addGalleryItemsListener() {
 }
 
 renderGalleryItems();
+createGalleryItem();
+onGalleryItemClick();
 addGalleryItemsListener();
+
 console.log(galleryItems);
