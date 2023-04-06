003ba35d3b16ddac9be5614756c0e1921218f4bd
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-04-06 15:12:38 +0200

    13

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,56 @@
+// import { galleryItems } from "./gallery-items.js";
+
+// import * as basicLightbox from "basiclightbox";
+// const gallery = document.querySelector(".gallery");
+
+// function renderGalleryItems(item) {
+//   const galleryItemsElements = galleryItems.map(createGalleryItem);
+//   function createGalleryItem() {
+//     const galleryItem = document.querySelector(".gallery__item");
+//     const image = galleryItem.querySelector(".gallery__image");
+//     const link = galleryItem.querySelector(".gallery__link");
+
+//     image.src = item.preview;
+//     image.dataset.source = item.original;
+//     image.alt = item.description;
+//     link.href = item.original;
+
+//     return galleryItem;
+//   }
+// }
+
+// function onGalleryItemClick(event) {
+//   event.preventDefault();
+//   const imageSrc = event.target.dataset.source;
+//   const instance = basicLightbox.create(`
+//     <img src="${imageSrc}" width="800" height="600">
+//   `);
+//   instance.show();
+// }
+
+// function addGalleryItemsListener() {
+//   gallery.addEventListener("click", (event) => {
+//     if (event.target.classList.contains("gallery__image")) {
+//       onGalleryItemClick(event);
+//     }
+//   });
+// }
+
+// renderGalleryItems();
+// createGalleryItem();
+// onGalleryItemClick();
+// addGalleryItemsListener();
+
+// console.log(galleryItems);
+
 import { galleryItems } from "./gallery-items.js";
 
 import * as basicLightbox from "basiclightbox";
 const gallery = document.querySelector(".gallery");
 
-function renderGalleryItems(item) {
+function renderGalleryItems() {
   const galleryItemsElements = galleryItems.map(createGalleryItem);
-  function createGalleryItem() {
+  function createGalleryItem(item) {
     const galleryItem = document.querySelector(".gallery__item");
     const image = galleryItem.querySelector(".gallery__image");
     const link = galleryItem.querySelector(".gallery__link");
@@ -14,6 +59,10 @@ function renderGalleryItems(item) {
     image.dataset.source = item.original;
     image.alt = item.description;
     link.href = item.original;
+
+    // Set the maximum width and height of the image
+    image.style.maxWidth = "800px";
+    image.style.maxHeight = "600px";
 
     return galleryItem;
   }
@@ -37,8 +86,5 @@ function addGalleryItemsListener() {
 }
 
 renderGalleryItems();
-createGalleryItem();
-onGalleryItemClick();
 addGalleryItemsListener();
-
 console.log(galleryItems);
