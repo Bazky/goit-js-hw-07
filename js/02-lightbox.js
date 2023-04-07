efd17044be36a82cc80582478b9c92710aa0cd9b
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-04-07 18:10:56 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.alt = item.description;
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+const gallery = document.querySelector(".gallery");
+const html = galleryItems.map(createGalleryItem);
+gallery.append(...html);
+
+const options = {
+  captions: true,
+  captionsDelay: 250,
+};
+
+const lightbox = new SimpleLightbox(".gallery a", options);
